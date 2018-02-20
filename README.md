a6b69bc93a296901722d34334ae1a290aace8fee
Sheina <ssim8@hawaii.edu>
2018-02-19 14:11:20 -1000

    Added responses, set environment, changed README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # magic8ball
 Python script that answers all of life's questions
+
+usage: ./magic8ball.py
--- magic8ball.py
+++ magic8ball.py
@@ -1,13 +1,14 @@
-# usage: python magic8ball.py
+#!/usr/bin/env python
+
 from random import *
 
 def magic8ball():
     question = raw_input("What is your question? ")
-    yesno_responses = ["It is certain.", "As Justin Trudeau sees it, yes.", "Reply hazy try again.", "Don't count on it.", "It is decidedly so.", "Justin Trudeau thinks so.", "Ask again later.", "Justin Trudeau's reply is no.", "Without a doubt.", "Outlook good.", "Better not tell you now.", "Justin Trudeau says no.", "Yes, definitely.", "Yes.", "Cannot predict now.", "Outlook not so good.", "You may rely on it", "Justin Trudeau points to yes.", "Concentrate and ask again.", "Justin Trudeau doesn't think so."]
+    yesno_responses = ["Justin Trudeau is certain.", "As Justin Trudeau sees it, yes.", "Reply hazy try again.", "Don't count on it.", "Justin Trudeau decided it so.", "Justin Trudeau thinks so.", "Justin Trudeau wants you to ask again later.", "Justin Trudeau's reply is no.", "Justin Trudeau has no doubt.", "Outlook good.", "Better not tell you now.", "Justin Trudeau says no.", "Yes, definitely.", "Yes.", "Cannot predict now.", "Outlook not so good.", "You may rely on it", "Justin Trudeau points to yes.", "Concentrate and ask Justin Trudeau again.", "Justin Trudeau doesn't think so."]
     responses = ["Grep it", "C1V1 = C2V2", "ssh 10.1.1.1", "What would Justin Trudeau do?", "Eat a taco.", "Look in your heart."]
     how_responses = ["Grep it", "ssh 10.1.1.1", "What would Justin Trudeau do?", "Look in your heart.", "One leg at a time."]
     who_responses = ["Justin Trudeau", "Your momma", "Who do you think?", "A loser", "A winner"]
-    whatis_responses = ["Tacos"]
+    whatis_responses = ["Tacos", "42"]
     if question.lower().startswith('is') or question.lower().startswith('should') or question.lower().startswith('could') or question.lower().startswith('will'):
         response_index = randint(0, len(yesno_responses)-1)
         print yesno_responses[response_index]
